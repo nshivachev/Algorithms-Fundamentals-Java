feb59a5b60daa97d334e6076f2bf3c5280cc7660
nshivachev <shivachev.nikolay@gmail.com>
2024-12-22 18:22:30 +0200

    Lab: Graph Theory, Traversal and Shortest Paths - Task1

---
+++ src/fundamentals/graphTheoryTraversalShortestPathsLab/ConnectedComponents.java
@@ -0,0 +1,63 @@
+package fundamentals.graphTheoryTraversalShortestPathsLab;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConnectedComponents {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            String input = scanner.nextLine();
+
+            if (input.trim().isEmpty()) {
+                graph.add(new ArrayList<>());
+                continue;
+            }
+
+            List<Integer> nextNodes = Arrays.stream(input.split("\\s+"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+            graph.add(nextNodes);
+        }
+
+        List<Deque<Integer>> connectedComponents = getConnectedComponents(graph);
+
+        for (Deque<Integer> connectedComponent : connectedComponents) {
+            System.out.print("Connected component: ");
+            for (int integer : connectedComponent) {
+                System.out.print(integer + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static List<Deque<Integer>> getConnectedComponents(List<List<Integer>> graph) {
+        boolean[] visited = new boolean[graph.size()];
+        List<Deque<Integer>> components = new ArrayList<>();
+
+        for (int start = 0; start < graph.size(); start++) {
+            if (!visited[start]) {
+                components.add(new ArrayDeque<>());
+                dfs(start, components, graph, visited);
+            }
+        }
+
+        return components;
+    }
+
+    public static void dfs(int node, List<Deque<Integer>> components, List<List<Integer>> graph, boolean[] visited) {
+        if (!visited[node]) {
+            visited[node] = true;
+            for (int child : graph.get(node)) {
+                dfs(child, components, graph, visited);
+            }
+            components.get(components.size() - 1).offer(node);
+        }
+    }
+}
